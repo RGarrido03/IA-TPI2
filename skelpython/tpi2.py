96d4cd763905e059c387d4e88bfacf5e1fb6c46d
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-08 11:46:53 +0000

    Incomplete query_local

--- skelpython/tpi2.py
+++ skelpython/tpi2.py
@@ -1,4 +1,5 @@
 # encoding: utf8
+from typing import Type
 
 # YOUR NAME: Rúben Tavares Garrido
 # YOUR NUMBER: 107927
@@ -9,19 +10,45 @@
 
 from semantic_network import *
 from constraintsearch import *
+from skelpython.semantic_network import Association
 
 
 class MySN(SemanticNetwork):
     def __init__(self):
         SemanticNetwork.__init__(self)
+        self.query_result = []
         # ADD CODE HERE IF NEEDED
         pass
 
     def query_local(self, user=None, e1=None, rel=None, e2=None):
-        # IMPLEMENT HERE
-        pass
-        return self.query_result  # Your code must leave the output in
-        # self.query_result, which is returned here
+        def get_decl(
+            user_iter: str, e1_iter: str, rel_iter: str, e2_iter: str | set
+        ) -> list[Declaration]:
+            if rel_iter == "member":
+                return [Declaration(user_iter, Member(e1_iter, e2_iter))]
+
+            if rel_iter == "subtype":
+                return [Declaration(user_iter, Subtype(e1_iter, e2_iter))]
+
+            if isinstance(e2_iter, str):
+                return [Declaration(user_iter, AssocOne(e1_iter, rel_iter, e2_iter))]
+
+            return [
+                Declaration(user_iter, Association(e1_iter, rel_iter, e2_iter_each))
+                for e2_iter_each in e2_iter
+            ]
+
+        self.query_result = [
+            d
+            for user_iter, v in self.declarations.items()
+            for (e1_iter, rel_iter), e2_iter in v.items()
+            for d in get_decl(user_iter, e1_iter, rel_iter, e2_iter)
+            if (user is None or user_iter == user)
+            and (e1 is None or e1_iter == e1)
+            and (rel is None or rel_iter == rel)
+            and (e2 is None or e2 in e2_iter)
+        ]
+        return self.query_result
 
     def query(self, entity, assoc=None):
         # IMPLEMENT HERE
