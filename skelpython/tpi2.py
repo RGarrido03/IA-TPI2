1a4c006d33dd1b9a71b213e4e215cffb885f1595
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-08 12:57:56 +0000

    Exercise 1a)

--- skelpython/tpi2.py
+++ skelpython/tpi2.py
@@ -19,7 +19,7 @@ class MySN(SemanticNetwork):
 
     def query_local(self, user=None, e1=None, rel=None, e2=None):
         def get_decl(
-            user_iter: str, e1_iter: str, rel_iter: str, e2_iter: str | set
+            user_iter: str, e1_iter: str, rel_iter: str, e2_iter: Union[str, set]
         ) -> list[Declaration]:
             if rel_iter == "member":
                 return [Declaration(user_iter, Member(e1_iter, e2_iter))]
@@ -41,9 +41,12 @@ class MySN(SemanticNetwork):
             for (e1_iter, rel_iter), e2_iter in v.items()
             for d in get_decl(user_iter, e1_iter, rel_iter, e2_iter)
             if (user is None or user_iter == user)
-            and (e1 is None or e1_iter == e1)
-            and (rel is None or rel_iter == rel)
-            and (e2 is None or e2 in e2_iter)
+            and (e1 is None or e1 == e1_iter)
+            and (rel is None or rel == rel_iter)
+            and (
+                e2 is None
+                or (e2 in e2_iter if isinstance(e2_iter, set) else e2 == e2_iter)
+            )
         ]
         return self.query_result
 
